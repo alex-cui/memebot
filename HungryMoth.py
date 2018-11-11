a47649404e296a8fd01e1eba771f4f300cd2b015
alex-cui <alexcui1@yahoo.com>
2018-11-11 12:02:35 -0800

    formatting

--- HungryMoth.py
+++ HungryMoth.py
@@ -1,5 +1,9 @@
 import praw
-bot = praw.Reddit(user_agent='HungryMothBot',client_id='Lf1RYS-DToojdQ', client_secret='beINITOxMTltT3hZQubyIS-aclY',username='HungryMothWantsLamp', password='GiveMeThatLamp')
+bot = praw.Reddit(user_agent='HungryMothBot',
+		  client_id='Lf1RYS-DToojdQ',
+		  client_secret='beINITOxMTltT3hZQubyIS-aclY',
+		  username='HungryMothWantsLamp', 
+		  password='GiveMeThatLamp')
 
 subreddit = bot.subreddit('popular')
 
