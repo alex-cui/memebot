7fd179901e9a2ca6276cb855a556585c9f1d322c
Patrick Reilly <patrick.reilly22@gmail.com>
2018-11-11 13:18:40 -0800

    Changed time limits

--- HungryMoth.py
+++ HungryMoth.py
@@ -1,4 +1,5 @@
 import praw
+import time
 bot = praw.Reddit(user_agent='HungryMothBot',client_id='Lf1RYS-DToojdQ', client_secret='beINITOxMTltT3hZQubyIS-aclY',username='HungryMothWantsLamp', password='GiveMeThatLamp')
 
 subreddit = bot.subreddit('popular')
@@ -8,9 +9,12 @@ comments = subreddit.stream.comments()
 for comment in comments:
     text = comment.body # Fetch body
     author = comment.author # Fetch author
-    if ' lamp ' in text.lower():
+    time.sleep(.1);
+    if (' lamp' in text.lower()) or ('light source' in text.lower()) or ("glowing" in text.lower()):
         # Generate a message
         message = "https://imgur.com/gallery/pJVKB2f I am hungry." 
-
+	print(text)
         comment.reply(message) # Send message
 	time.sleep(1)
+    else:
+    	print("Nope!")
