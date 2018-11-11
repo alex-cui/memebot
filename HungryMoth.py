d7bb41b776f60652f3822ba902788bb3b907cc93
Patrick Reilly <patrick.reilly22@gmail.com>
2018-11-11 10:20:25 -0800

    Added Auth code

--- HungryMoth.py
+++ HungryMoth.py
@@ -1,2 +1,7 @@
 import praw
-user_agent = "I am a hungry moth "
+user_agent = ("I am a hungry moth ")
+r = praw.Reddit(user_agent=user_agent)
+thing_limit = 10
+user_name = "HungryMothWantsLamp"
+user = r.get_redittor(user_name)
+
