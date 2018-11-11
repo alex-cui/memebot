d1d45c74e1df8fa07e119f6fe12b9bfb8f18a341
alex-cui <alexcui1@yahoo.com>
2018-11-11 11:10:13 -0800

    rough draft

--- HungryMoth.py
+++ HungryMoth.py
@@ -1,7 +1,20 @@
-import praw
-user_agent = ("I am a hungry moth ")
-r = praw.Reddit(user_agent=user_agent)
-thing_limit = 10
-user_name = "HungryMothWantsLamp"
-user = r.get_redittor(user_name)
+import praw # simple interface to the reddit API, also handles rate limiting of requests
 
+bot = praw.Reddit(user_agent='I am a hungry moth',
+                  client_id='MothBot',
+                  client_secret='',
+                  username='HungryMothWantsLamp',
+                  password='')
+
+subreddit = bot.subreddit('popular')
+
+comments = subreddit.stream.comments()
+
+for comment in comments:
+    text = comment.body # Fetch body
+    author = comment.author # Fetch author
+    if 'Moth' or 'moth' in text.lower():
+        # Generate a message
+        message = "".format(author) 
+
+        comment.reply(message) # Send message
