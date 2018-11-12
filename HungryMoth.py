45e238c63e573f41a8b4bddd75889ff2c4679e84
alex-cui <alexcui1@yahoo.com>
2018-11-11 18:52:33 -0800

    optimized

--- HungryMoth.py
+++ HungryMoth.py
@@ -2,11 +2,11 @@ import praw
 import time
 
 class Meme:
-    
+    #constructor
     def __init__(self, terms, response, name):
-    	self.response = response
-	self.terms = []
-	self.name = name
+        self.response = response
+        self.terms = []
+        self.name = name
 
     def addTerm(self, term):
     	self.terms.append(term)
@@ -21,13 +21,12 @@ lampMoth = Meme([],"https://imgur.com/gallery/pJVKB2f I am hungry.", "Lamp/Moth"
 lampMoth.addTerm(" lightbulb ")
 lampMoth.addTerm(" bright ")
 lampMoth.addTerm(" lamp ")
-lampMoth.addTerm("light source")
+lampMoth.addTerm(" moon ")
 lampMoth.addTerm(" glowing ")
-#lampMoth.setResponse("https://imgur.com/gallery/pJVKB2f I am hungry.")
+lampMoth.setResponse("https://imgur.com/gallery/pJVKB2f I am hungry.")
 
 shrekDonkey = Meme([], "https://66.media.tumblr.com/1e2f561f44d85b1219fd4008ea843578/tumblr_mvc7jbak6Q1sm2yoxo4_250.gif", "Shrek/Donkey")
-#shrekDonkey.setResponse("https://66.media.tumblr.com/1e2f561f44d85b1219fd4008ea843578/tumblr_mvc7jbak6Q1sm2yoxo4_250.gif")
-shrekDonkey.addTerm(" ogre ")
+shrekDonkey.setResponse("https://66.media.tumblr.com/1e2f561f44d85b1219fd4008ea843578/tumblr_mvc7jbak6Q1sm2yoxo4_250.gif")
 shrekDonkey.addTerm(" ogres ")
 shrekDonkey.addTerm(" onions ")
 shrekDonkey.addTerm(" layers ")
@@ -35,34 +34,33 @@ shrekDonkey.addTerm(" parfait ")
 
 Memes = [lampMoth, shrekDonkey]
 
+bot = praw.Reddit(user_agent='HungryMothBot',client_id='Lf1RYS-DToojdQ', client_secret='beINITOxMTltT3hZQubyIS-aclY',username='HungryMothWantsLamp', password='GiveMeThatLamp')
 
-bot = praw.Reddit(user_agent='Reddit MemeBot by u/HungryMothWantsLamp',client_id='Lf1RYS-DToojdQ', client_secret='beINITOxMTltT3hZQubyIS-aclY',username='HungryMothWantsLamp', password='GiveMeThatLamp')
-
-subreddit = bot.subreddit('all')
+subreddit = bot.subreddit('popular')
 
 comments = subreddit.stream.comments()
 
 moveOn = False
 desired_meme_num = -1
-while( not moveOn):
-	print("Meme List:")
-	for meme in Memes:
-		print(meme.name)
 
-	print("What number meme do you want to auto-reply to? ")
-	desired_meme_num = input();
-	if desired_meme_num > -1 and desired_meme_num < len(Memes):
+while(not moveOn):
+    print("\nMeme List:")
+    for meme in Memes:
+        print("    " + meme.name)
+
+    print("\nWhat number meme do you want to auto-reply to? ")
+    desired_meme_num = int(input())
+    if desired_meme_num > -1 and desired_meme_num < len(Memes):
 	    moveOn = True    
+
+print("\nSearching...")
 
 for comment in comments:
     text = comment.body # Fetch body
     author = comment.author # Fetch author
-    time.sleep(.01)
-    for key in Memes[desired_meme_num].terms: 
-	if key in text.lower():
-           #comment.reply(Memes[desired_meme_num].respond()) # Send message
-	    print(text)   
-	    time.sleep(1)
-    else:
-    	print("Nope!")
 
+    for key in Memes[desired_meme_num].terms:
+        if key in text.lower():
+                print("Success!")
+                comment.reply(Memes[desired_meme_num].respond()) # Send message
+                time.sleep(1)
